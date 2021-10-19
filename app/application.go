2a67737b1fa2d035b49902606a1f6a17c67e8cca
Sora8d <miguelpx2012@gmail.com>
2021-10-19 12:05:01 -0300

    PORT alone 2

--- app/application.go
+++ app/application.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Sora8d/bookstore_utils-go/logger"
@@ -9,13 +10,14 @@ import (
 )
 
 var router = mux.NewRouter()
-var port = config.Config["port"]
+var address = fmt.Sprintf("%s:%s", config.Config["address"], config.Config["port"])
 
 func StartApplication() {
 	Urlmaps()
+	logger.Info(fmt.Sprintf("starting app, trying connection in: %s", address))
 	srv := &http.Server{
 		Handler: router,
-		Addr:    config.Config["address"] + port,
+		Addr:    address,
 	}
 	if err := srv.ListenAndServe(); err != nil {
 		logger.Error("error starting app", err)
